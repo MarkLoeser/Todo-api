5ddb3ffdd51ba0b4eb2d81bde51c79da45fd59c7
Mark Loeser <mark.loeser@gmail.com>
2016-12-14 06:43:20 -0500

    add todos and todos/id

--- server.js
+++ server.js
@@ -1,9 +1,49 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+	id: 1,
+	description: 'Feed Oliver',
+	completed: false,
+}, {
+	id: 2,
+	description: "go to market",
+	completed: false
+},
+{
+	id: 3,
+	description: "learn node.js",
+	completed: true
+}]
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
+});
+
+//GET /todos
+
+app.get('/todos', function (req, res) {
+	res.json(todos);
+});
+
+//GET /todos/:id  todos/3
+app.get('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo;
+	
+	todos.forEach(function (todo) {
+		if (todoId === todo.id) {
+			matchedTodo = todo;	
+		}
+	});
+
+	if (matchedTodo){
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
+
+
 });
 
 app.listen(PORT, function () {
